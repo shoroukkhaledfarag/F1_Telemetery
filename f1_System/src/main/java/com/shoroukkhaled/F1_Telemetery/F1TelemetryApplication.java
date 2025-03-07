553a2ce326d8c5a60824b166ed510be83a0c077d
shorouk.khaled <shorouk.khaled@gizasystems.com>
2025-03-07 22:43:22 +0200

    init commit

---
+++ f1_System/src/main/java/com/shoroukkhaled/F1_Telemetery/F1TelemetryApplication.java
@@ -0,0 +1,12 @@
+package com.shoroukkhaled.F1_Telemetery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1TelemetryApplication {
+
+        public static void main(String[] args) {
+            SpringApplication.run(F1TelemetryApplication.class, args);
+        }
+}
